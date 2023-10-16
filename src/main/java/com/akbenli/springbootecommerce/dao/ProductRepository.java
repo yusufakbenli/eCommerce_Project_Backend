d2ceaee961ef80ec33293d2b76058c0bb2b92b00
yusufakbenli <akbenliyusuf@gmail.com>
2023-10-16 11:16:53 +0300

    Release 1.0-Product Service

---
+++ src/main/java/com/akbenli/springbootecommerce/dao/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.akbenli.springbootecommerce.dao;
+
+import com.akbenli.springbootecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product,Long>
+{
+
+}
+
